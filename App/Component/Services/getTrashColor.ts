301374cec64ac61fc2b0d7a4f59ba48b70cd3687
William risal <william.risal@epitech.eu>
2023-01-21 16:53:00 +0100

    Update getTrashColor.ts

--- App/Component/Services/getTrashColor.ts
+++ App/Component/Services/getTrashColor.ts
@@ -5,7 +5,7 @@ export const getTrashColor = (trash: any): string => {
     if (trash.productData?.product?.ecoscore_data?.adjustments?.packaging?.non_recyclable_and_non_biodegradable_materials == 1) {
         console.log('Marron');
     }
-    //parcour le json et trouve la matiere
+
     for (let itempack, i = 0; itempack = trash.productData?.product?.ecoscore_data?.adjustments?.packaging.packagings[i]; i++) {
         for (let item, j = 0; item = whatcolortrash.poubelles[j]; j++) {
             if (item.exemples.includes(itempack.material)) {
@@ -13,15 +13,6 @@ export const getTrashColor = (trash: any): string => {
             }
         }
     }
-    // for (let i = 0; i < whatcolortrash.poubelles.length; i++) {
-    //     for (let j = 0; j < whatcolortrash.poubelles[i].exemples.length; j++) {
-    //         if (whatcolortrash.poubelles[i].exemples[j] === trash.productData?.product?.ecoscore_data?.adjustments?.packaging.packagings[0].material) {
-    //             console.log(whatcolortrash.poubelles[i].couleur);          
-    //         }
-    //     }
-    // }
-    //si la matiere n'est pas dans le json
-
     console.log("//////////////:")
 
 }

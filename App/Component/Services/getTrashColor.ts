4e54f9d3ccb521c95b14b012d5a338bdc4192e34
William risal <william.risal@epitech.eu>
2023-01-21 18:58:36 +0100

    Update getTrashColor.ts

--- App/Component/Services/getTrashColor.ts
+++ App/Component/Services/getTrashColor.ts
@@ -1,18 +1,22 @@
 import whatcolortrash from '../../Data/whatcolortrash.json';
-export const getTrashColor = (trash: any): string => {
-    console.log("//////////////:")
-    console.log(trash.productData?.product?.ecoscore_data?.adjustments?.packaging.packagings[0].material);
-    if (trash.productData?.product?.ecoscore_data?.adjustments?.packaging?.non_recyclable_and_non_biodegradable_materials == 1) {
-        console.log('Marron');
-    }
 
-    for (let itempack, i = 0; itempack = trash.productData?.product?.ecoscore_data?.adjustments?.packaging.packagings[i]; i++) {
-        for (let item, j = 0; item = whatcolortrash.poubelles[j]; j++) {
-            if (item.exemples.includes(itempack.material)) {
-                console.log(item.couleur);
+export const getTrashColor = (trash: any): string => {
+    let packaging = {};
+
+    if (trash.productData?.product?.ecoscore_data?.adjustments?.packaging?.non_recyclable_and_non_biodegradable_materials == 1 && trash.productData?.product?.ecoscore_data?.adjustments?.packaging?.warning ) {
+        Object.assign(packaging, {"all": "nodata"});
+    }
+    else if (trash.productData?.product?.ecoscore_data?.adjustments?.packaging?.non_recyclable_and_non_biodegradable_materials == 1) {
+        Object.assign(packaging, {"all": "Marron"});
+    }
+    else {
+        for (let itempack, i = 0; itempack = trash.productData?.product?.ecoscore_data?.adjustments?.packaging.packagings[i]; i++) {
+            for (let item, j = 0; item = whatcolortrash.poubelles[j]; j++) {
+                if (item.exemples.includes(itempack.material)) {
+                    Object.assign(packaging, {[itempack.shape] : item.couleur});
+                }
             }
         }
     }
-    console.log("//////////////:")
 
 }

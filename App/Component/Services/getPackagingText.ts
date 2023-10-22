a8eab3a4dc6f2fa4bf4a59edd7c1bf3984169ec8
William risal <63760727+williamrisal@users.noreply.github.com>
2023-10-22 16:27:58 +0200

    Update getPackagingText.ts

--- App/Component/Services/getPackagingText.ts
+++ App/Component/Services/getPackagingText.ts
@@ -4,10 +4,13 @@ export const getPackagingText = (trash: any) => {
         trash.data &&
         trash.data.productData &&
         trash.data.productData.product &&
-        typeof trash.data.productData.product.packaging_text_fr !== ""
+        trash.data.productData.product.packaging_text_fr &&
+        trash.data.productData.product.packaging_text_fr.length > 0
     ) {
         return trash.data.productData.product.packaging_text_fr;
+    } else if (trash.data.productData.product.packaging_text && trash.data.productData.product.packaging_text.length > 0) {
+        return trash.data.productData.product.packaging_text;
     } else {
-        return null; 
+        return null;
     }
 }
